037e5eb0c87768abf34ec799fb474e180281b602
XianEnLin <blueeye851114@gmail.com>
2021-11-01 23:32:59 +0800

    搜尋清除多餘空白、優化程式碼

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json').results
 
 const app = express()
 const port = 3000
 
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurantList: restaurantList.results })
+  res.render('index', { restaurantList })
 })
 
 app.get('/search', (req, res) => {
-  const { keyword } = req.query
-  const searchResult = restaurantList.results.filter(r => 
-    r.name.toLowerCase().includes(keyword.toLowerCase()) ||
-    r.category.toLowerCase().includes(keyword.toLowerCase())
+  const keyword = req.query.keyword.trim().toLowerCase()
+  if (!keyword) {
+    res.redirect('/')
+    return
+  }
+  const searchResult = restaurantList.filter(r => 
+    r.name.toLowerCase().includes(keyword) ||
+    r.category.toLowerCase().includes(keyword)
   )
   const isNoResult = searchResult.length === 0
+
   res.render('index', { restaurantList: searchResult, keyword, isNoResult })
 })
 
 app.get('/restaurants/:restaurantId', (req, res) => {
   const { restaurantId } = req.params
-  const restaurant = restaurantList.results.find(r => r.id.toString() === restaurantId)
+  const restaurant = restaurantList.find(r => r.id.toString() === restaurantId)
 
   res.render('show', { restaurant })
 })
 
 app.listen(port, () => {
-  console.log('Listening on http://localhost:3000')
+  console.log(`Listening on http://localhost:${port}`)
 })
