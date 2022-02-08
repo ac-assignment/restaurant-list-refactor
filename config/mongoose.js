fc88a5a47cea38732bded2785d2df2f42457ae63
XianEnLin <blueeye851114@gmail.com>
2022-02-08 23:36:58 +0800

    style: 專案名稱微調

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/restaurant_list')
+mongoose.connect('mongodb://localhost:27017/restaurant-list')
 const db = mongoose.connection
 
 db.on('error', () => {
